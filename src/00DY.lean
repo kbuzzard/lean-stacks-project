9b38a5044a351eb55fb5ef57029bc58c34eee590
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-02-19 23:05:03 +0000

    adding more lean files

---
+++ src/00DY.lean
@@ -0,0 +1,3 @@
+/-
+This is just 00DZ, 00E0, 00E1 and 00E2 and 00E3 and 00E4 and 00E5 and 00E6 and 00E7 and 00E8 and 04PM
+-/
---
+++ src/00E4.lean
@@ -0,0 +1,7 @@
+/-
+Lemma 10.16.6. Let R be a ring. Let f∈R. The map R→Rf induces via the functoriality of Spec a homeomorphism
+Spec(Rf)⟶D(f)⊂Spec(R).
+The inverse is given by 𝔭↦𝔭⋅Rf.
+
+Proof. This is a special case of Lemma 10.16.5 (=tag 00E3)
+-/
