c78a243a53f4249b71d8312dab5f53d024b3bc3c
kckennylau <kc_kennylau@yahoo.com.hk>
2018-02-20 13:53:16 +0000

    00C7

--- src/00C6.lean
+++ src/00C6.lean
@@ -13,6 +13,6 @@ variables {α : Type u} [comm_ring α] (S : set α) [is_submonoid α S]
 instance god : add_comm_group α := ring.to_add_comm_group α
 instance why : add_group α := by apply_instance
 
-theorem subsingleton_iff_zero_mem : subsingleton (localization.loc α S) ↔ (0:α) ∈ S :=
+theorem localization.subsingleton_iff_zero_mem : subsingleton (localization.loc α S) ↔ (0:α) ∈ S :=
 ⟨λ ⟨h⟩, let ⟨w, H, hw⟩ := quotient.exact (h 0 1) in by simp at hw; rwa hw at H,
  λ h, ⟨λ x y, quotient.induction_on₂ x y $ λ m n, quotient.sound $ ⟨0, h, by simp⟩⟩⟩
