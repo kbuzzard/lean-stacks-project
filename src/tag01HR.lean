8ff3c3fff6286d8f27a2e19aa38f99edcb6b7ff9
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-02-27 00:06:17 +0000

    objects of presheaf now defined

--- src/tag01HR.lean
+++ src/tag01HR.lean
@@ -15,21 +15,19 @@ universe u
 
 def is_zariski.standard_open {R : Type u} [comm_ring R] (U : set (X R)) := ∃ f : R, U = Spec.D'(f)
 
-set_option pp.notation false
 def nonzero_on_U_is_mult_set {R : Type u} [comm_ring R] (U : set (X R)) : is_submonoid R {g : R | U ⊆ Spec.D'(g)} := 
-{ one_mem := λ P HP, show P.val 1 → false,by simp, -- dammit P is a prime ideal of course it doens't contain 1
+{ one_mem := λ P HP, @is_proper_ideal.one_not_mem _ _ P.1 P.2.1,
   mul_mem := begin
     intros f g Hf Hg,
     show U ⊆ Spec.D'(f*g),
     rw [tag00E0.lemma15 R f g],
     exact set.subset_inter Hf Hg
     end
-
 }
-  --sorry -- follows from lemma15 00E0
+
 def zariski.structure_sheaf_standard {R : Type u} [comm_ring R] (U : set (X R)) (H : is_zariski.standard_open U) : Type u := 
   @localization.loc R _ {g : R | U ⊆ Spec.D'(g)} (nonzero_on_U_is_mult_set U)
 
 lemma zariski.structure_sheaf_on_standard {R : Type u} [comm_ring R] (f : R) : let U := Spec.D'(f) in
   zariski.structure_sheaf_standard U (⟨f,rfl⟩) = localization.loc R (powers f) := sorry -- they are supposed to be canonically isomorphic not equal
-  
+
