3e56d39ad432fac92939f1b6c7e302cd9db2e63e
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-03-24 21:26:27 +0000

    adding new empty tag

---
+++ src/tag009N.lean
@@ -0,0 +1,29 @@
+/-
+\begin{lemma}
+\label{lemma-extend-off-basis}
+Let $X$ be a topological space.
+Let $\mathcal{B}$ be a basis for the topology on $X$.
+Let $\mathcal{F}$ be a sheaf of sets on $\mathcal{B}$.
+There exists a unique sheaf of sets $\mathcal{F}^{ext}$
+on $X$ such that $\mathcal{F}^{ext}(U) = \mathcal{F}(U)$
+for all $U \in \mathcal{B}$ compatibly with the restriction
+mappings.
+\end{lemma}
+
+\begin{proof}
+We first construct a presheaf $\mathcal{F}^{ext}$ with the
+desired property. Namely, for an arbitrary open $U \subset X$ we
+define $\mathcal{F}^{ext}(U)$ as the set of elements
+$(s_x)_{x \in U}$ such that $(*)$ of
+Lemma \ref{lemma-condition-star-sections} holds.
+It is clear that there are restriction mappings
+that turn $\mathcal{F}^{ext}$ into a presheaf of sets.
+Also, by Lemma \ref{lemma-condition-star-sections} we
+see that $\mathcal{F}(U) = \mathcal{F}^{ext}(U)$
+whenever $U$ is an element of the basis $\mathcal{B}$.
+To see $\mathcal{F}^{ext}$ is a sheaf one may
+argue as in the proof of Lemma \ref{lemma-sheafification-sheaf}.
+\end{proof}
+-/
+
+-- sheaf on a basis = sheaf on the whole spaces
