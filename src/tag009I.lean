d7a788465a76934d3ac05a7cd40249212f300db0
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-03-25 13:06:53 +0100

    making some stuff implict

--- src/tag009I.lean
+++ src/tag009I.lean
@@ -7,9 +7,9 @@ import analysis.topology.topological_space
 
 structure presheaf_of_types_on_basis {X : Type*} [TX : topological_space X] {B : set (set X)}
   (HB : topological_space.is_topological_basis B) := 
-(F : Π U : set X, B U → Type*)
+(F : Π {U : set X}, B U → Type*)
 (res : ∀ {U V : set X} (OU : B U) (OV : B V) (H : V ⊆ U), 
-  (F U OU) → (F V OV))
+  (F OU) → (F OV))
 (Hid : ∀ (U : set X) (OU : B U), (res OU OU (set.subset.refl U)) = id)  
 (Hcomp : ∀ (U V W : set X) (OU : B U) (OV : B V) (OW : B W)
   (HUV : V ⊆ U) (HVW : W ⊆ V),
@@ -21,6 +21,6 @@ structure morphism_of_presheaves_of_types_on_basis {X : Type*} [TX : topological
   (FPT : presheaf_of_types_on_basis HB) 
   (GPT : presheaf_of_types_on_basis HB) 
   :=
-(morphism : ∀ U : set X, ∀ HU : B U, (FPT.F U HU) → GPT.F U HU)
+(morphism : ∀ U : set X, ∀ HU : B U, (FPT.F HU) → GPT.F HU)
 (commutes : ∀ U V : set X, ∀ HU : B U, ∀ HV : B V, ∀ Hsub : V ⊆ U,
   (GPT.res HU HV Hsub) ∘ (morphism U HU) = (morphism V HV) ∘ (FPT.res HU HV Hsub))
