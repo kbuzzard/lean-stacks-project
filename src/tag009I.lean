567f05ed85731ab67e55f6902d3716271cd7ff59
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-03-25 12:34:16 +0100

    made some sets implict

--- src/tag009I.lean
+++ src/tag009I.lean
@@ -8,12 +8,12 @@ import analysis.topology.topological_space
 structure presheaf_of_types_on_basis {X : Type*} [TX : topological_space X] {B : set (set X)}
   (HB : topological_space.is_topological_basis B) := 
 (F : Π U : set X, B U → Type*)
-(res : ∀ (U V : set X) (OU : B U) (OV : B V) (H : V ⊆ U), 
+(res : ∀ {U V : set X} (OU : B U) (OV : B V) (H : V ⊆ U), 
   (F U OU) → (F V OV))
-(Hid : ∀ (U : set X) (OU : B U), (res U U OU OU (set.subset.refl U)) = id)  
+(Hid : ∀ (U : set X) (OU : B U), (res OU OU (set.subset.refl U)) = id)  
 (Hcomp : ∀ (U V W : set X) (OU : B U) (OV : B V) (OW : B W)
   (HUV : V ⊆ U) (HVW : W ⊆ V),
-  (res U W OU OW (set.subset.trans HVW HUV)) = (res V W OV OW HVW) ∘ (res U V OU OV HUV) )
+  (res OU OW (set.subset.trans HVW HUV)) = (res OV OW HVW) ∘ (res OU OV HUV) )
 
 
 structure morphism_of_presheaves_of_types_on_basis {X : Type*} [TX : topological_space X] 
@@ -23,4 +23,4 @@ structure morphism_of_presheaves_of_types_on_basis {X : Type*} [TX : topological
   :=
 (morphism : ∀ U : set X, ∀ HU : B U, (FPT.F U HU) → GPT.F U HU)
 (commutes : ∀ U V : set X, ∀ HU : B U, ∀ HV : B V, ∀ Hsub : V ⊆ U,
-  (GPT.res U V HU HV Hsub) ∘ (morphism U HU) = (morphism V HV) ∘ (FPT.res U V HU HV Hsub))
+  (GPT.res HU HV Hsub) ∘ (morphism U HU) = (morphism V HV) ∘ (FPT.res HU HV Hsub))
