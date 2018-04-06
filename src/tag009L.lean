9f962609dd25745861c84332481989b6c79a9f69
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-04-06 19:56:00 +0100

    started

--- src/tag009L.lean
+++ src/tag009L.lean
@@ -1,14 +1,15 @@
 import tag009J 
+universes u v w uu vv ww uuu vvv www 
 
 definition sheaf_property 
-  {X : Type*} [T : topological_space X] 
+  {X : Type u} [T : topological_space X] 
   {B : set (set X)} 
   (HB : topological_space.is_topological_basis B)
   (FPTB : presheaf_of_types_on_basis HB)
   (Hstandard : ∀ {U V : set X}, B U → B V → B (U ∩ V))
   (U : set X)
   (BU : B U)
-  (γ : Type*)
+  (γ : Type u)
   --(Hfingamma: fintype γ) -- only interested in finite covers here
   (Ui : γ → set X)
   (BUi : ∀ i : γ, B (Ui i))
@@ -19,20 +20,19 @@ definition sheaf_property
   → ∃! s : FPTB.F BU, ∀ i : γ, FPTB.res BU (BUi i) (Hcover ▸ (set.subset_Union Ui i)) s = si i 
 
 definition basis_is_compact 
-  {X : Type*} [T : topological_space X] 
+  {X : Type u} [T : topological_space X] 
   {B : set (set X)} 
   (HB : topological_space.is_topological_basis B) : Prop :=
   ∀ U : set X, B U →
-    ∀ β : Type*, ∀ Ui : β → set X,
+    ∀ β : Type u, ∀ Ui : β → set X,
     (∀ i : β, B (Ui i)) → 
     (⋃ (i : β), Ui i) = U →
-    ∃ γ : Type*, ∃ Hfinite : fintype γ,
+    ∃ γ : Type u, ∃ Hfinite : fintype γ,
     ∃ f : γ → β, 
     (⋃ (j : γ), Ui (f j)) = U 
 
-
 theorem lemma_cofinal_systems_coverings_standard_case 
-  {X : Type*} [T : topological_space X] 
+  {X : Type u} [T : topological_space X] 
   {B : set (set X)} 
   (HB : topological_space.is_topological_basis B)
   (FPTB : presheaf_of_types_on_basis HB)
@@ -41,19 +41,32 @@ theorem lemma_cofinal_systems_coverings_standard_case
   (HBcompact: basis_is_compact HB)
   : 
   (∀ U : set X, ∀ BU : B U,
-  ∀ γ : Type*, fintype γ → -- note fintype here
+  ∀ γ : Type u, fintype γ → -- note fintype here
   ∀ Ui : γ → set X,
   ∀ BUi :  ∀ i : γ, B (Ui i),
   ∀ Hcover: (⋃ (i : γ), (Ui i)) = U,
   sheaf_property HB FPTB Hstandard U BU γ Ui BUi Hcover)
   → 
   (∀ U : set X, ∀ BU : B U,
-  ∀ γ : Type*, 
+  ∀ γ : Type u, 
   ∀ Ui : γ → set X,
   ∀ BUi :  ∀ i : γ, B (Ui i),
   ∀ Hcover: (⋃ (i : γ), (Ui i)) = U,  
   sheaf_property HB FPTB Hstandard U BU γ Ui BUi Hcover)
-  := sorry 
+  := 
+begin
+  intro Hfinite,
+  intros U BU β Ui BUi Hcover,
+  intros si Hsiglue,
+  -- Hcover has a finite subcover
+  cases HBcompact U BU β Ui BUi Hcover with γ Hγ,
+  cases Hγ with HFinγ Hf,
+  cases Hf with f Hfincover,
+  have X := Hfinite U BU γ HFinγ (Ui ∘ f) (λ j, BUi (f j)) Hfincover,
+  
+  admit,
+end
+
 
   -- the **only property of fintype you will need here**
   -- is basis_is_compact; you definitely do not need
