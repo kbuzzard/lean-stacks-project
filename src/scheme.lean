e78988c4716e65fa39a2b45e04d08a767e3feed6
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-04-12 11:46:44 +0100

    final fixes after lean upgrade

--- src/scheme.lean
+++ src/scheme.lean
@@ -2,7 +2,7 @@ import analysis.topology.topological_space data.set
 import analysis.topology.continuity 
 import Kenny_comm_alg.Zariski
 import Kenny_comm_alg.temp
-import tag00EJ_statement
+import tag00EJ
 import localization
 import localization_UMP
 import tag00E0
@@ -325,7 +325,6 @@ structure scheme :=
       (structure_presheaf_of_rings_on_affine_scheme R)
 )
 
-#print axioms scheme.mk
 /-
 definition presheaf_of_rings_pullback_under_open_immersion
   {α : Type*} [Tα : topological_space α]
