4e1e67eaf2f7017a0ffc63278a4241f74b3008ed
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-05-03 23:48:10 +0100

    tidy

--- scratch/scratch1.lean
+++
@@ -1,7 +0,0 @@
-theorem W : 0 = 1 → false := dec_trivial 
-
-theorem X : 1000 * 1000 + 0 = 1000 * 1000 + 1 → false := λ H, W (nat.add_left_cancel H)
-
-example : 1000 * 1000 + 0 = 1000 * 1000 + 1 := rfl -- no problem
-
-#eval 2+2
