cecc4cb6dcd4a62415405136986511dd31bcd55b
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-05-07 00:33:23 +0100

    do I care about this issue

---
+++ scratch/hash_check_issue.lean
@@ -0,0 +1,9 @@
+import data.equiv
+#check equiv.left_inv
+#check set.image -- I don't like ?M_1, no clue as to type
+#check @set.image -- oh if I'd known they were alpha and beta
+                  -- I would have known they were types. 
+variables (α β : Type)
+#check @set.image α β -- That's what I wanted to see the first time
+
+#check equiv
