b0ecc5818df60d44ca69dcf52bb98e1ecae249af
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-05-23 08:21:34 +0100

    Kenny was right about function composition

--- src/tag006E.lean
+++ src/tag006E.lean
@@ -10,6 +10,22 @@ structure presheaf_of_types (α : Type u) [T : topological_space α] :=
   (HUV : V ⊆ U) (HVW : W ⊆ V),
   (res U W OU OW (set.subset.trans HVW HUV)) = (res V W OV _ HVW) ∘ (res U V _ _ HUV) )
 
+@[simp] lemma presheaf_of_types.Hcomp' {X : Type u} [TX : topological_space X]
+  (FPT : presheaf_of_types X) :
+∀ {U V W : set X} (OU : TX.is_open U) (OV : TX.is_open V) (OW : TX.is_open W)
+  (HUV : V ⊆ U) (HVW : W ⊆ V) (s : FPT.F OU),
+  (FPT.res U W OU OW (set.subset.trans HVW HUV)) s = 
+    (FPT.res V W OV OW HVW) ((FPT.res U V OU OV HUV) s) := begin
+    intros U V W OU OV OW HUV HVW s,
+      show _ = ((FPT.res V W OV OW HVW) ∘ (FPT.res U V OU OV HUV)) s,
+      rw FPT.Hcomp,
+    end
+
+@[simp] lemma presheaf_of_types.Hid' {X : Type u} [TX : topological_space X]
+  (FPT : presheaf_of_types X) :
+∀ {U : set X} (OU : TX.is_open U) (s : FPT.F OU),
+  (FPT.res U U OU OU (set.subset.refl U)) s = s := λ U OU s,by rw FPT.Hid U OU;simp
+
 structure morphism_of_presheaves_of_types {α : Type u} [Tα : topological_space α] 
   (FPT : presheaf_of_types α) (GPT : presheaf_of_types α) :=
 (morphism : ∀ U : set α, ∀ HU : Tα.is_open U, (FPT.F HU) → GPT.F HU)
